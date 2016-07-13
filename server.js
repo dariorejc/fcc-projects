e9c1d5cfc0a5a5711126ab5e72d4e2e58e53a771
Dario Rejc <dario.rejc@gmail.com>
2016-07-13 11:53:49 +0000

    Final working version

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var millisToStr = function(number) {
+    var montharray=["January","February","March","April","May","June", "July","August","September","October","November","December"];
+    var date = new Date(number * 1000);
+    var dateStr = "";
+    
+    dateStr += montharray[date.getMonth()] + " " + date.getDay() + ", " + date.getFullYear();
+    
+    return dateStr;
+}
+
+app.get('/:time', function (req, res) {
+    var str = decodeURIComponent(req.params.time);
+    var dateFmt = parseInt(str, 10);
+    var date = isNaN(dateFmt) ? new Date(str) : new Date(dateFmt);
+    
+    if (date.toString() === "Invalid Date") {
+        res.send({"unix" : null, "natural" : null});
+    } else {
+        if (isNaN(str)) {
+            res.send({"unix" : date.valueOf(), "natural" : str});
+        } else {
+            res.send({"unix" : date.valueOf(), "natural" : millisToStr(str)});
+        }
+        
+    }
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Timestamp app listening on port 8080!');
 });
