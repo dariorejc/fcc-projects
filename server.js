e3d897a13a92c33aabbcd87ed78569240ce5476c
Dario <dario.rejc@gmail.com>
2016-08-09 10:01:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.get('/:time', function (req, res) {
     }
 });
 
-app.listen(8080, function () {
+app.listen(process.env.PORT || 8080, function () {
   console.log('Timestamp app listening on port 8080!');
 });
